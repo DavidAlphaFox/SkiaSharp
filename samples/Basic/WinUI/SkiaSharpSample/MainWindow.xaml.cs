1667b896df3021c872b662b694471879bf61f825
Matthew Leibowitz <mattleibow@live.com>
2024-01-12 01:36:19 +0200

    WinUI has GPU!

--- samples/Basic/WinUI/SkiaSharpSample/MainWindow.xaml.cs
+++ samples/Basic/WinUI/SkiaSharpSample/MainWindow.xaml.cs
@@ -33,5 +33,28 @@ namespace SkiaSharpSample
 			var coord = new SKPoint(e.Info.Width / 2, (e.Info.Height + font.Size) / 2);
 			canvas.DrawText("SkiaSharp", coord, SKTextAlign.Center, font, paint);
 		}
+
+		private void OnPaintSurface(object sender, SKPaintGLSurfaceEventArgs e)
+		{
+			// the the canvas and properties
+			var canvas = e.Surface.Canvas;
+
+			// make sure the canvas is blank
+			canvas.Clear(SKColors.White);
+
+			// draw some text
+			using var paint = new SKPaint
+			{
+				Color = SKColors.Black,
+				IsAntialias = true,
+				Style = SKPaintStyle.Fill
+			};
+			using var font = new SKFont
+			{
+				Size = 24
+			};
+			var coord = new SKPoint(e.Info.Width / 2, (e.Info.Height + font.Size) / 2);
+			canvas.DrawText("SkiaSharp", coord, SKTextAlign.Center, font, paint);
+		}
 	}
 }
